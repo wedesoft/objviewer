d32b47e7d0419c6623c9d4c8b7b57907480cb17b
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-09-24 14:01:34 +0100

    Bind buffer globally

--- fsim.c
+++ fsim.c
@@ -4,18 +4,14 @@
 #include <GL/glut.h>
 
 
-GLuint vbo;
-
 void onDisplay(void)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-  glBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo);
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_FLOAT, 0, 0);
   glDrawArrays(GL_TRIANGLES, 0, 3);
   glDisableClientState(GL_VERTEX_ARRAY);
-  glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
 
   glutSwapBuffers();
 }
@@ -37,6 +33,7 @@ int main(int argc, char** argv)
 
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+  GLuint vbo;
   glGenBuffersARB(1, &vbo);
   glBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo);
   glBufferDataARB(GL_ARRAY_BUFFER_ARB, sizeof(vertices), 0, GL_STATIC_DRAW_ARB);
@@ -45,6 +42,7 @@ int main(int argc, char** argv)
   glutDisplayFunc(onDisplay);
   glutMainLoop();
 
+  glBindBufferARB(GL_ARRAY_BUFFER_ARB, 0);
   glDeleteBuffersARB(1, &vbo);
   return 0;
 }
