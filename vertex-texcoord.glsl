79fede42f9f4314e4254377d2880effa6da45873
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-10-12 13:03:50 +0100

    Added missing vertex-texcoord file

---
+++ vertex-texcoord.glsl
@@ -0,0 +1,9 @@
+#version 300 es
+layout(location = 0) in mediump vec3 point;
+layout(location = 1) in mediump vec2 texcoord;
+out mediump vec2 UV;
+void main()
+{
+  gl_Position = vec4(point, 1);
+  UV = texcoord;
+}
