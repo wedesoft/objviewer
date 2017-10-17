fcea76a2b50c73ebb4e0a1d987a80c9e1af0aa3f
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-10-17 15:02:46 +0100

    Created trivial unit test

--- test-fsim.c
+++ test-fsim.c
@@ -1,6 +1,29 @@
 #include "munit.h"
 
-int main(int argc, char *atgv[MUNIT_ARRAY_PARAM(argc + 1)])
+static MunitResult test_it(const MunitParameter params[], void *data)
 {
-  return 0;
+  munit_assert(1);
+  return MUNIT_OK;
+}
+
+static MunitTest test_suite_tests[] = {
+  "/test",
+  test_it,
+  NULL,
+  NULL,
+  MUNIT_TEST_OPTION_NONE,
+  NULL
+};
+
+static MunitSuite test_suite[] = {
+  "",
+  test_suite_tests,
+  NULL,
+  1,
+  MUNIT_SUITE_OPTION_NONE
+};
+
+int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)])
+{
+  return munit_suite_main(&test_suite[0], (void*)NULL, argc, argv);
 }
