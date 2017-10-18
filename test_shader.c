59b05403ce4d7e752cfcf495f585d5f5957fda6d
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-10-18 20:48:27 +0100

    Test shader syntax error

--- test_shader.c
+++ test_shader.c
@@ -15,8 +15,15 @@ static MunitResult test_compile_shader(const MunitParameter params[], void *data
   return MUNIT_OK;
 }
 
+static MunitResult test_shader_syntax_error(const MunitParameter params[], void *data)
+{
+  munit_assert_ptr(make_shader(GL_VERTEX_SHADER, "invalid.glsl"), ==, NULL);
+  return MUNIT_OK;
+}
+
 MunitTest test_shader[] = {
   {"/shader_file_not_found", test_shader_file_not_found, test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
   {"/compile_shader"       , test_compile_shader       , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
+  {"/shader_syntax_error"  , test_shader_syntax_error  , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
   {NULL                    , NULL                      , NULL         , NULL            , MUNIT_TEST_OPTION_NONE, NULL}
 };
