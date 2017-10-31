11f20439b82e3e00c291bcb192c7d0e3cfdc9814
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-10-31 21:27:13 +0000

    Removed unused shaders

--- projection.glsl
+++
@@ -1,7 +0,0 @@
-#version 130
-in mediump vec3 point;
-uniform mat4 projection;
-void main()
-{
-  gl_Position = projection * vec4(point, 1);
-}
--- white.glsl
+++
@@ -1,6 +0,0 @@
-#version 130
-out mediump vec3 fragColor;
-void main()
-{
-  fragColor = vec3(1, 1, 1);
-}
