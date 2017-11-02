918f44075dffd96093913373fd0517a442770578
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-02 21:02:04 +0000

    Use parser in integration tests

--- tests/test_integration.c
+++ tests/test_integration.c
@@ -1,6 +1,7 @@
 #include "test_integration.h"
 #include "test_helper.h"
 #include "fsim/object.h"
+#include "fsim/parser.h"
 #include "fsim/program.h"
 #include "fsim/projection.h"
 #include "fsim/vertex_array_object.h"
@@ -8,16 +9,17 @@
 
 static MunitResult test_draw_triangle(const MunitParameter params[], void *data)
 {
-  surface_t *surface = make_surface();
-  add_vertex_data(surface, 3,  0.5f,  0.5f, 0.0f);
-  add_vertex_data(surface, 3, -0.5f,  0.5f, 0.0f);
-  add_vertex_data(surface, 3, -0.5f, -0.5f, 0.0f);
-  add_triangle(surface, 0, 1, 2);
+  object_t *object =
+    parse_string("o triangle\n"
+                 "v  0.5  0.5 0\n"
+                 "v -0.5  0.5 0\n"
+                 "v -0.5 -0.5 0\n"
+                 "s off\n"
+                 "f 1 2 3");
+  surface_t *surface = get_pointer(object->surface)[0];
   program_t *program = make_program("vertex-identity.glsl", "fragment-blue.glsl");
   vertex_array_object_t *vertex_array_object = make_vertex_array_object(program, surface);
   setup_vertex_attribute_pointer(vertex_array_object, "point", 3, 3);
-  object_t *object = make_object("test");
-  add_surface(object, surface);
   list_t *list = make_list();
   append_pointer(list, vertex_array_object);
   glViewport(0, 0, (GLsizei)width, (GLsizei)height);
@@ -37,59 +39,24 @@ static MunitResult test_draw_triangle(const MunitParameter params[], void *data)
   return MUNIT_OK;
 }
 
-static MunitResult test_draw_two_surfaces(const MunitParameter params[], void *data)
-{
-  float coord[] = {0.5f, -0.5f};
-  const char *fragment_shader_file_name[] = {"fragment-blue.glsl", "fragment-red.glsl"};
-  object_t *object = make_object("test");
-  list_t *list = make_list();
-  int i;
-  for (i=0;i<2; i++) {
-    surface_t *surface = make_surface();
-    float c = coord[i];
-    add_vertex_data(surface, 3,     c,     c, 0.0f);
-    add_vertex_data(surface, 3, -0.5f,  0.5f, 0.0f);
-    add_vertex_data(surface, 3,  0.5f, -0.5f, 0.0f);
-    add_triangle(surface, 0, 1, 2);
-    program_t *program = make_program("vertex-identity.glsl", fragment_shader_file_name[i]);
-    vertex_array_object_t *vertex_array_object = make_vertex_array_object(program, surface);
-    setup_vertex_attribute_pointer(vertex_array_object, "point", 3, 3);
-    add_surface(object, surface);
-    append_pointer(list, vertex_array_object);
-  };
-  glViewport(0, 0, (GLsizei)width, (GLsizei)height);
-  glClearColor(0, 1, 0, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
-  render(list);
-  glFinish();
-  unsigned char *pixels = read_pixels();
-  write_ppm("draw_surfaces.ppm", width, height, pixels);
-  munit_assert_int(pixels[0], ==,   0);
-  munit_assert_int(pixels[1], ==, 255);
-  munit_assert_int(pixels[2], ==,   0);
-  munit_assert_int(pixels[( 8 * 32 + 14 ) * 4 + 0], ==, 255);
-  munit_assert_int(pixels[( 8 * 32 + 14 ) * 4 + 1], ==,   0);
-  munit_assert_int(pixels[( 8 * 32 + 14 ) * 4 + 2], ==,   0);
-  munit_assert_int(pixels[(12 * 32 + 18 ) * 4 + 0], ==,   0);
-  munit_assert_int(pixels[(12 * 32 + 18 ) * 4 + 1], ==,   0);
-  munit_assert_int(pixels[(12 * 32 + 18 ) * 4 + 2], ==, 255);
-  return MUNIT_OK;
-}
-
 static MunitResult test_use_normal(const MunitParameter params[], void *data)
 {
-  surface_t *surface = make_surface();
-  add_vertex_data(surface, 6,  0.5f,  0.5f, 0.0f,  0.0f,  0.0f, 1.0f);
-  add_vertex_data(surface, 6, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 0.0f);
-  add_vertex_data(surface, 6, -0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f);
-  add_triangle(surface, 0, 1, 2);
+  object_t *object =
+    parse_string("o normals\n"
+                 "v  0.5  0.5 0\n"
+                 "v -0.5  0.5 0\n"
+                 "v -0.5 -0.5 0\n"
+                 "vn 0 0 1\n"
+                 "vn 0 1 0\n"
+                 "vn 1 0 0\n"
+                 "s off\n"
+                 "f 1//1 2//2 3//3");
+  surface_t *surface = get_pointer(object->surface)[0];
   program_t *program = make_program("vertex-normal-identity.glsl", "fragment-normal.glsl");
   vertex_array_object_t *vertex_array_object = make_vertex_array_object(program, surface);
   setup_vertex_attribute_pointer(vertex_array_object, "point" , 3, 6);
   setup_vertex_attribute_pointer(vertex_array_object, "vector", 3, 6);
-  object_t *object = make_object("test");
   list_t *list = make_list();
-  add_surface(object, surface);
   append_pointer(list, vertex_array_object);
   glViewport(0, 0, (GLsizei)width, (GLsizei)height);
   glClearColor(0, 0, 0, 1);
@@ -106,20 +73,25 @@ static MunitResult test_use_normal(const MunitParameter params[], void *data)
 
 static MunitResult test_draw_texturized_square(const MunitParameter params[], void *data)
 {
-  surface_t *surface = make_surface();
-  add_vertex_data(surface, 5, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f);
-  add_vertex_data(surface, 5,  0.5f, -0.5f, 0.0f, 1.0f, 0.0f);
-  add_vertex_data(surface, 5, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f);
-  add_vertex_data(surface, 5,  0.5f,  0.5f, 0.0f, 1.0f, 1.0f);
-  add_triangle(surface, 0, 1, 2); extend_triangle(surface, 3);
+  object_t *object =
+    parse_string("o texturized\n"
+                 "v -0.5 -0.5 0\n"
+                 "v  0.5 -0.5 0\n"
+                 "v -0.5  0.5 0\n"
+                 "v  0.5  0.5 0\n"
+                 "vt 0 0\n"
+                 "vt 1 0\n"
+                 "vt 0 1\n"
+                 "vt 1 1\n"
+                 "s off\n"
+                 "f 1/1 2/2 4/4 3/3");
+  surface_t *surface = get_pointer(object->surface)[0];
   program_t *program = make_program("vertex-texcoord.glsl", "fragment-texture.glsl");
   vertex_array_object_t *vertex_array_object = make_vertex_array_object(program, surface);
   setup_vertex_attribute_pointer(vertex_array_object, "point"             , 3, 5);
   setup_vertex_attribute_pointer(vertex_array_object, "texture_coordinate", 2, 5);
   add_texture(vertex_array_object, make_texture("tex"), read_image("colors.png"));
-  object_t *object = make_object("test");
   list_t *list = make_list();
-  add_surface(object, surface);
   append_pointer(list, vertex_array_object);
   glViewport(0, 0, (GLsizei)width, (GLsizei)height);
   glClearColor(0, 0, 0, 1);
@@ -139,16 +111,17 @@ static MunitResult test_draw_texturized_square(const MunitParameter params[], vo
 
 static MunitResult test_perspective_triangle(const MunitParameter params[], void *data)
 {
-  surface_t *surface = make_surface();
-  add_vertex_data(surface, 3,  0.5f,  0.5f, -1.0f);
-  add_vertex_data(surface, 3, -0.5f,  0.5f, -1.0f);
-  add_vertex_data(surface, 3, -0.5f, -0.5f, -1.0f);
-  add_triangle(surface, 0, 1, 2);
+  object_t *object =
+    parse_string("o perspective\n"
+                 "v  0.5  0.5 -1\n"
+                 "v -0.5  0.5 -1\n"
+                 "v -0.5 -0.5 -1\n"
+                 "s off\n"
+                 "f 1 2 3");
+  surface_t *surface = get_pointer(object->surface)[0];
   program_t *program = make_program("vertex-projection.glsl", "fragment-blue.glsl");
   vertex_array_object_t *vertex_array_object = make_vertex_array_object(program, surface);
   setup_vertex_attribute_pointer(vertex_array_object, "point", 3, 3);
-  object_t *object = make_object("test");
-  add_surface(object, surface);
   list_t *list = make_list();
   append_pointer(list, vertex_array_object);
   glViewport(0, 0, (GLsizei)width, (GLsizei)height);
@@ -168,7 +141,6 @@ static MunitResult test_perspective_triangle(const MunitParameter params[], void
 
 MunitTest test_integration[] = {
   {"/draw_triangle"          , test_draw_triangle          , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
-  {"/draw_two_surfaces"      , test_draw_two_surfaces      , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
   {"/use_normal"             , test_use_normal             , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
   {"/draw_texturized_square" , test_draw_texturized_square , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
   {"/perspective_triangle"   , test_perspective_triangle   , test_setup_gl, test_teardown_gl, MUNIT_TEST_OPTION_NONE, NULL},
